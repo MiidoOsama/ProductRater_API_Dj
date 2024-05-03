6181e4cf428d10e096b8ba939955df47ad976dd4
MiidoOsama <xmiido@yandex.com>
2024-05-03 17:04:31 +0300

    first commit

---
+++ api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator , MinValueValidator
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=360)
+    
+    def __str__(self):
+        return self.name
+
+class Rating(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.meal
+    
+    class Meta : 
+        unique_together = (('user', 'meal'),)
+        index_together = (('user', 'meal'),)
+
+
+
